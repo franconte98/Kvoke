3851f8e4fe44d679f37e978e4c9f55f32ecbc435
franconte <FrancescoConte@mapsgroup.it>
2025-08-26 12:47:18 +0200

    Improve UI

---
+++ texts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### --- Import Variables ---
+source ./start.sh
+
+WELCOME_MESSAGE="This script will guide you through the initial setup and preliminary configuration of a Kubernetes cluster.
+This IaC script suite automates Kubernetes cluster installation and configuration. 
+It includes three official node-joining configurations, adaptable to your available infrastructure.\n\n
+
+VM Prerequisites:
+- Each VM requires a unique IP address.
+- All VMs must reside on the same network (and must be reachable for each other).
+- At least 2vCPU, 2Gi of TAM and 40Gb of storage for each VM.
+- All the VMs got to have the same sudoer user (With the same username and password)."
+
+WELCOME_1="This cluster configuration requires less infrastructure, but it's less focused on high availability (NON-HA). [~ 10 minutes to create it]
+
+\nMINIMUM REQUIREMENTS:
+- 1 Master Node
+- 1, up to 10 Worker Nodes
+\nNETWORK PREREQUISITES:- Each VM requires a unique IP address.
+- All VMs must reside on the same network (and must be reachable for each other).
+- At least 2vCPU, 2Gi of RAM and 40Gb of Storage for each VM.
+- All the VMs got to have the same sudoer user (With the same username and password)."
+
+CONFIRM_1="Adopted configuration: Basic Non-High Availability Setup\n
+Number of Working Nodes: $count_workers\n
+Master IP $ip_master\n
+$dialog_text
+IPAddressPool for LB: $show_range\n
+Container Runtime: $cri\n\n
+Credentials SSH non-ROOT:\n
+Username: $username
+Password: $passwd
+"
